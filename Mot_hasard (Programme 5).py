c400efbbabafac72e5b54dcd86e5a9507ed83261
Enzopaspris <90576415+Enzopaspris@users.noreply.github.com>
2021-10-04 10:54:17 +0200

    Add files via upload

---
+++ Mot_hasard (Programme 5).py
@@ -0,0 +1,3 @@
+mot=input("Rentez un mot de votre choix")
+for i in range(len(mot)):
+    print(chr(ord(mot[i])+8))
---
+++ Nb_ordi 10 essaies.py
@@ -0,0 +1,21 @@
+﻿from random import randint
+
+nb_ordi=randint(1,100)
+print("nb_ordi :",nb_ordi)
+nb_essais=1
+nb_utilisateur=-1
+print("Tu as 10 essais pour trouver le nombre entre 1 et 100")
+
+while nb_ordi!=nb_utilisateur and nb_essais<11:
+    nb_utilisateur=int(input("Quel est le nombre ?"))
+    print("essai :",nb_essais)
+    if(nb_utilisateur<nb_ordi):
+        print("C'est plus")
+    elif(nb_utilisateur>nb_ordi):
+        print("C'est moins")
+    nb_essais=nb_essais+1
+
+if nb_ordi==nb_utilisateur:
+    print("Bravo tu as touvé en",nb_essais-1,"essais")
+else:
+    print("Dommage le nombre était le",nb_ordi)
