91f9a494c009bf4e4854a6a4daa01045d1372e64
Enzopaspris <90576415+Enzopaspris@users.noreply.github.com>
2021-10-07 09:00:50 +0200

    Delete Année Bissextile.py

--- Année Bissextile.py
+++
@@ -1,8 +0,0 @@
-﻿annee=int(input("Donne moi une année supérieur a 1582, je te dirais si oui ou non elle est bissextile"))
-if annee>=1582:
-    if (annee%4==0 and not annee%100==0) or annee%400==0:
-        print(annee,"est une année bissextile")
-    else :
-        print(annee,"n'est pas une année bissextile ")
-else:
-    print("L'année que tu as choisis est inférieur a 1582")
