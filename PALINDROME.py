afe90bf935caf32651a990d356856171cf8bc8b7
Enzopaspris <90576415+Enzopaspris@users.noreply.github.com>
2021-11-17 21:55:04 +0100

    Add files via upload

---
+++ PALINDROME.py
@@ -0,0 +1,12 @@
+"""def palin(N):
+    if L<=0 : return True
+    N1 = N // 10**L
+    N2 = JSP
+    L = ogd(N) - 2
+    if N1 == N2 :
+        return palin((N-N1*10**L) // 10)"""
+
+def ogd(N):
+    return len(str(N)- 1)
+
+palin(1)
