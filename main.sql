9dc74eded15ef911cef74e49483e38dd4a61e89d
Enzopaspris <90576415+Enzopaspris@users.noreply.github.com>
2021-12-09 09:58:17 +0100

    Add files via upload

---
+++ main.sql
@@ -0,0 +1,24 @@
+-- create a table
+CREATE TABLE elevesNSI (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  gender TEXT NOT NULL,
+  prenom TEXT NOT NULL,
+  naissance INT
+);
+INSERT INTO elevesNSI VALUES (1, 'ALLI', 'M', 'Zéphir', NULL);
+INSERT INTO elevesNSI VALUES (2, 'BAR', 'M', 'Jules', 3);
+INSERT INTO elevesNSI VALUES (3, 'BENARDE', 'M', 'Quentin', 11);
+INSERT INTO elevesNSI VALUES (4, 'BUKOV', 'M', 'Noa', 2);
+INSERT INTO elevesNSI VALUES (5, 'GABORIE', 'M', 'César', 4);
+INSERT INTO elevesNSI VALUES (6, 'GUENGA', 'M', 'Noe', 10);
+INSERT INTO elevesNSI VALUES (7, 'GUILBAU', 'M', 'Ethan', 5);
+INSERT INTO elevesNSI VALUES (8, 'HAMA', 'M', 'Louis', 3);
+INSERT INTO elevesNSI VALUES (9, 'LE GUYAD', 'M', 'Nattan', 10);
+INSERT INTO elevesNSI VALUES (10, 'MARTIN', 'M', 'Enzo', 11);
+INSERT INTO elevesNSI VALUES (11, 'MICET', 'M', 'Thibaut', 6);
+INSERT INTO elevesNSI VALUES (12, 'PROUTIE', 'M', 'Ethan', 2);
+INSERT INTO elevesNSI VALUES (13, 'RADOU', 'M', 'Youssef', 6);
+INSERT INTO elevesNSI VALUES (14, 'SALOU', 'M', 'Clement', 4);
+INSERT INTO elevesNSI VALUES (15, 'VER', 'M', 'Salif', 7);
+INSERT INTO elevesNSI VALUES (16, 'VIGNE', 'M', 'Alexandre', 5  );
