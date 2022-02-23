18ac2ea59df8aa49cb93251e3ec6c8c08237e32f
hmmmm les doonuts <90576415+Enzopaspris@users.noreply.github.com>
2022-02-23 21:25:38 +0100

    Add files via upload

--- ABR_liste.py
+++ ABR_liste.py
@@ -1,11 +1,24 @@
-AB = [1, 2, 3, 4, 5, 6, 7]
+def creation_ABR(taille):
+    L = [None for i in range(taille + 1)]
+    return L
 
 def recherche(L, e, n=0):
-    if L[n] == e :
-        print("L'élément est présent")
-    elif L[2*n + 1] == e or L[2*n + 2] == e:
-        print("L'élément est présent")
-    else :
-        recherche(L, e, n =+ 1)
+    if len(L) > 0:
+        if L[n] == e :
+            return True
+        elif L[2*n ] == e or L[2*n + 1] == e:
+            return True
+        else :
+            recherche(L, e, n =+ 1)
 
-recherche(AB, 8)
+def insertion(L, e):
+    if L[0] == None :
+        L[0] = e
+    else :
+
+
+
+
+
+L = creation_ABR(3)
+print(L)
